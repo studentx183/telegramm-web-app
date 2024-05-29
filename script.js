12c3146c47838b8507812362081b2a7493d6e2e8
rustam <studentx183@tuit.uz>
2024-05-29 13:38:43 +0500

    adopting to user theme

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const onClickSubmitBtn = () => {
 };
 
 const adoptToUserTheme = () => {
-  const userInfoDiv = document.createElement('div')
-  const main = document.querySelector('main');
+  const userInfoDiv = document.createElement("div");
+  userInfoDiv.id = 'adsjaklsdjalkjdlkajdlkjalkjdkljaklsdjalkjsduirjrfnu'
+  const main = document.querySelector("main");
   userInfoDiv.textContent = JSON.stringify(DemoApp.userTheme);
   main.appendChild(userInfoDiv);
   // --------------------------------
@@ -153,8 +154,21 @@ const initYandexMap = () => {
   }
 };
 
+// init user theme
+const initUserThemeAdopt = () => {
+  DemoApp.init();
+  DemoApp.userTheme = Telegram.WebApp.ThemeParams;
+  adoptToUserTheme();
+  Telegram.WebApp.onEvent("themeChanged", function () {
+    DemoApp.userTheme = Telegram.WebApp.ThemeParams;
+    adoptToUserTheme();
+  });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   const formContent = document.getElementById("addForm");
+
+  initUserThemeAdopt();
 
   // Data for regions and cities
   const data = {
@@ -218,7 +232,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const select = document.createElement("select");
     const label = document.createElement("label");
     label.textContent = _label;
-    label.style.color = DemoApp.userTheme.button_color
+    label.style.color = DemoApp.userTheme?.button_color;
     select.name = id;
     select.id = id;
     select.required = true;
@@ -541,17 +555,5 @@ const DemoApp = {
       .catch(function (error) {
         onCallback && onCallback({ error: "Server error" });
       });
-  },
-};
-
-const DemoAppInitData = {
-  init() {
-    DemoApp.init();
-    Telegram.WebApp.onEvent("themeChanged", function () {
-      DemoApp.userTheme = Telegram.WebApp.ThemeParams;
-      adoptToUserTheme();
-    });
-    DemoApp.userTheme = Telegram.WebApp.ThemeParams;
-    adoptToUserTheme();
   },
 };
